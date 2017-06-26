c9c5524ef4b5d63ebf3041d2ea2f99d6ee9bd5ef
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 22:30:34 +0200

    refactor logic from CarAdvertController to a more generalized ControllerBase

--- src/Artemis.Web/Controllers/CarAdvertController.cs
+++ src/Artemis.Web/Controllers/CarAdvertController.cs
@@ -11,79 +11,12 @@ using System.Web.Http.Cors;
 namespace Artemis.Web.Controllers
 {
     [EnableCors(origins: "*", headers: "*",  methods: "*")]
-    public class CarAdvertController : ApiController
+    public class CarAdvertController : ControllerBase<CarAdvert, CarAdvertViewModel>
     {
-        private IRepository<CarAdvert> repository;
-        private IMapper mapper;
-
         public CarAdvertController(IRepository<CarAdvert> repository, IMapper mapper)
+            : base(repository, mapper)
         {
-            this.repository = repository;
-            this.mapper = mapper;
-        }
-
-        public IHttpActionResult Get(string orderBy = null, string direction = null)
-        {
-            if (ModelState.IsValid)
-            {
-                var carAdverts = repository.Get(orderBy, direction);
-                var container = new CollectionResult<CarAdvertViewModel>()
-                {
-                    Entities = carAdverts.Select(c => mapper.Map<CarAdvertViewModel>(c))
-                };
-                return Ok(container);
-            }
-            return BadRequest();
-        }
-
-        public IHttpActionResult Get(int id)
-        {
-            if (ModelState.IsValid)
-            {
-                var carAdvert = repository.Get(id);
-                if (carAdvert == null)
-                    return NotFound();
-                return Ok(mapper.Map<CarAdvertViewModel>(carAdvert));
-            }
-            return BadRequest();
-        }
-
-        public IHttpActionResult Put(CarAdvertViewModel vm)
-        {
-            if (ModelState.IsValid)
-            {
-                var carAdvert = repository.Get(vm.Id);
-                if (carAdvert == null)
-                    return NotFound();
-                mapper.Map(vm, carAdvert);
-                repository.Update(carAdvert);
-                return Ok(mapper.Map<CarAdvertViewModel>(carAdvert));
-            }
-            return BadRequest();
-        }
-
-        public IHttpActionResult Post(CarAdvertViewModel vm)
-        {
-            if (ModelState.IsValid)
-            {
-                var carAdvert = mapper.Map<CarAdvert>(vm);
-                repository.Create(carAdvert);
-                return Ok(mapper.Map<CarAdvertViewModel>(carAdvert));
-            }
-            return BadRequest();
-        }
-
-        public IHttpActionResult Delete(CarAdvertViewModel vm)
-        {
-            if (ModelState.IsValid)
-            {
-                var carAdvert = repository.Get(vm.Id);
-                if (carAdvert == null)
-                    return NotFound();
-                repository.Delete(carAdvert);
-                return Ok();
-            }
-            return BadRequest();
+            
         }
     }
 }
---
+++ src/Artemis.Web/Controllers/ControllerBase.cs
@@ -0,0 +1,88 @@
+﻿using Artemis.Common;
+using Artemis.Web.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace Artemis.Web.Controllers
+{
+    public abstract class ControllerBase<TModel, TViewModel> : ApiController
+        where TViewModel : class, IHasKey
+    {
+        private IRepository<TModel> repository;
+        private IMapper mapper;
+
+        public ControllerBase(IRepository<TModel> repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public IHttpActionResult Get(string orderBy = null, string direction = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var entities = repository.Get(orderBy, direction);
+                var container = new CollectionResult<TViewModel>()
+                {
+                    Entities = entities.Select(c => mapper.Map<TViewModel>(c))
+                };
+                return Ok(container);
+            }
+            return BadRequest();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = repository.Get(id);
+                if (entity == null)
+                    return NotFound();
+                return Ok(mapper.Map<TViewModel>(entity));
+            }
+            return BadRequest();
+        }
+
+        public IHttpActionResult Put(TViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = repository.Get(vm.Id);
+                if (entity == null)
+                    return NotFound();
+                mapper.Map(vm, entity);
+                repository.Update(entity);
+                return Ok(mapper.Map<TViewModel>(entity));
+            }
+            return BadRequest();
+        }
+
+        public IHttpActionResult Post(TViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = mapper.Map<TModel>(vm);
+                repository.Create(entity);
+                return Ok(mapper.Map<TViewModel>(entity));
+            }
+            return BadRequest();
+        }
+
+        public IHttpActionResult Delete(TViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = repository.Get(vm.Id);
+                if (entity == null)
+                    return NotFound();
+                repository.Delete(entity);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
