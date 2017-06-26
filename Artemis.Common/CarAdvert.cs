3e87f612badd1ca72a900dd492a49bbb74ebbf13
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 08:34:06 +0200

    add project Artemis.Common with CarAdvert type

---
+++ Artemis.Common/CarAdvert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artemis.Common
+{
+    public class CarAdvert
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public FuelType Fuel { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public int? Mileage { get; set; }
+
+        public DateTime? FirstRegistration { get; set; }
+    }
+}
---
+++ Artemis.Common/FuelType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artemis.Common
+{
+    public enum FuelType
+    {
+        Default = 0,
+        Gasoline = 1,
+        Diesel = 2
+    }
+}
