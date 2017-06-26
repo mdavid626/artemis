2acbd21f71c77453f0a049ad6e5debfa16838543
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 09:20:29 +0200

    add static references to SQLite

--- src/Artemis.Data/CarAdvertDbContext.cs
+++ src/Artemis.Data/CarAdvertDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Artemis.Common;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,36 @@ namespace Artemis.Data
 {
     internal class CarAdvertDbContext : DbContext
     {
+        public CarAdvertDbContext() : base("SqlConnectionString")
+        {
+
+        }
+
+        public CarAdvertDbContext(DbConnection connection, bool contextOwnsConnection) 
+            : base(connection, contextOwnsConnection)
+        {
+
+        }
+
+        static CarAdvertDbContext()
+        {
+            var type = typeof(System.Data.Entity.SqlServer.SqlProviderServices);
+            if (type == null)
+                throw new Exception("Do not remove, ensures static reference to System.Data.Entity.SqlServer");
+
+            type = typeof(System.Data.SQLite.SQLiteContext);
+            if (type == null)
+                throw new Exception("Do not remove, ensures static reference to SQLite");
+
+            type = typeof(System.Data.SQLite.EF6.SQLiteProviderFactory);
+            if (type == null)
+                throw new Exception("Do not remove, ensures static reference to SQLite");
+
+            type = typeof(System.Data.SQLite.Linq.SQLiteProviderFactory);
+            if (type == null)
+                throw new Exception("Do not remove, ensures static reference to SQLite");
+        }
+
         public DbSet<CarAdvert> CarAdverts { get; set; }
     }
 }
