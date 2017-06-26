d444f737282888650469e37c28497fc726b7775b
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 12:28:07 +0200

    add tests for CarAdvertRepository

--- src/Artemis.Data.Tests/CarAdvertRepositoryTest.cs
+++ src/Artemis.Data.Tests/CarAdvertRepositoryTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Linq;
+using Artemis.Common;
 
 namespace Artemis.Data.Tests
 {
@@ -7,9 +10,56 @@ namespace Artemis.Data.Tests
     public class CarAdvertRepositoryTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestValidGet()
         {
+            using (var connection = Effort.DbConnectionFactory.CreateTransient())
+            {
+                // arrange
+                var dbContext = new CarAdvertDbContext(connection, false);
+                var carAdvert = new CarAdvert()
+                {
+                    Title = "Audi"
+                };
+                dbContext.CarAdverts.Add(carAdvert);
+                dbContext.SaveChanges();
 
+                var dbContextProvider = Substitute.For<ICarAdvertDbContextProvider>();
+                dbContextProvider.Provide().Returns(p => dbContext);
+                var repository = new CarAdvertRepository(dbContextProvider);
+
+                // act
+                var items = repository.Get().ToList();
+                var firstItem = items.FirstOrDefault();
+
+                // assert
+                Assert.AreEqual(firstItem.Title, carAdvert.Title);
+            }
+        }
+
+        [TestMethod]
+        public void TestValidGetById()
+        {
+            using (var connection = Effort.DbConnectionFactory.CreateTransient())
+            {
+                // arrange
+                var dbContext = new CarAdvertDbContext(connection, false);
+                var carAdvert = new CarAdvert()
+                {
+                    Title = "Audi"
+                };
+                dbContext.CarAdverts.Add(carAdvert);
+                dbContext.SaveChanges();
+
+                var dbContextProvider = Substitute.For<ICarAdvertDbContextProvider>();
+                dbContextProvider.Provide().Returns(p => dbContext);
+                var repository = new CarAdvertRepository(dbContextProvider);
+
+                // act
+                var item = repository.Get(carAdvert.Id);
+
+                // assert
+                Assert.AreEqual(item.Title, carAdvert.Title);
+            }
         }
     }
 }
