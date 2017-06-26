fc9ce398671dbe1d12f2f8039166320212f87f4f
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 08:21:36 +0200

    add properties to CarAdvert

--- src/Artemis.Web/Model/CarAdvert.cs
+++ src/Artemis.Web/Model/CarAdvert.cs
@@ -7,6 +7,18 @@ namespace Artemis.Web.Model
 {
     public class CarAdvert
     {
+        public int Id { get; set; }
 
+        public string Title { get; set; }
+
+        public string Fuel { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool New { get; set; }
+
+        public int? Mileage { get; set; }
+
+        public DateTime? FirstRegistration { get; set; }
     }
 }
