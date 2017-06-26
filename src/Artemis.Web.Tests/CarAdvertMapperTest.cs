c1c8054d93777fd1e805d5f159e92df6014442b3
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 12:00:31 +0200

    add more unit tests for CarAdvertMapperTest

--- src/Artemis.Web.Tests/CarAdvertMapperTest.cs
+++ src/Artemis.Web.Tests/CarAdvertMapperTest.cs
@@ -16,10 +16,69 @@ namespace Artemis.Web.Tests
             vm.Title = "Audi";
             vm.Price = 5000;
             vm.Fuel = "gasoline";
-            vm.New = false;
+            vm.New = true;
             var carAdvert = new CarAdvert();
             var result = vm.MapTo(carAdvert);
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMissingFuel()
+        {
+            var vm = new CarAdvertViewModel();
+            vm.Id = 1;
+            vm.Title = "Audi";
+            vm.Price = 5000;
+            vm.New = true;
+            var carAdvert = new CarAdvert();
+            var result = vm.MapTo(carAdvert);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestInvalidFuel()
+        {
+            var vm = new CarAdvertViewModel();
+            vm.Id = 1;
+            vm.Title = "Audi";
+            vm.Price = 5000;
+            vm.Fuel = "adsfdasf";
+            vm.New = true;
+            var carAdvert = new CarAdvert();
+            var result = vm.MapTo(carAdvert);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestUsedCar()
+        {
+            var vm = new CarAdvertViewModel();
+            vm.Id = 1;
+            vm.Title = "Audi";
+            vm.Price = 5000;
+            vm.Fuel = "diesel";
+            vm.New = false;
+            vm.Mileage = 5000;
+            vm.FirstRegistration = DateTime.Now;
+            var carAdvert = new CarAdvert();
+            var result = vm.MapTo(carAdvert);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestInvalidUsedCar()
+        {
+            var vm = new CarAdvertViewModel();
+            vm.Id = 1;
+            vm.Title = "Audi";
+            vm.Price = 5000;
+            vm.Fuel = "diesel";
+            vm.New = true;
+            vm.Mileage = 5000;
+            vm.FirstRegistration = DateTime.Now;
+            var carAdvert = new CarAdvert();
+            var result = vm.MapTo(carAdvert);
+            Assert.IsFalse(result);
         }
     }
 }
