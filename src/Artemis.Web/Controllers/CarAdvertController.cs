f4dc9b13590fb250ebe134a944608fbf83bd5523
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 08:06:56 +0200

    add empty CarAdvertController

---
+++ src/Artemis.Web/Controllers/CarAdvertController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace Artemis.Web.Controllers
+{
+    public class CarAdvertController : ApiController
+    {
+        public CarAdvertController()
+        {
+            
+        }
+    }
+}
