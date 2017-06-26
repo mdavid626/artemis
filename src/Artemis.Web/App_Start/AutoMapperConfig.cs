f93b2ce2fe26accb3c36f6bfa1c0fcd40282067c
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 22:28:44 +0200

    fix null reference errors while converting reg date

--- src/Artemis.Web/App_Start/AutoMapperConfig.cs
+++ src/Artemis.Web/App_Start/AutoMapperConfig.cs
@@ -17,11 +17,11 @@ namespace Artemis.Web
                 cfg.CreateMap<CarAdvert, CarAdvertViewModel>()
                    .ForMember(d => d.New, opt => opt.MapFrom(src => src.IsNew))
                    .ForMember(d => d.Fuel, opt => opt.MapFrom(src => src.Fuel.ToString().ToLower()))
-                   .ForMember(d => d.FirstRegistration, opt => opt.MapFrom(src => src.FirstRegistration.Value.Date));
+                   .ForMember(d => d.FirstRegistration, opt => opt.MapFrom(src => src.FirstRegistration.HasValue ? src.FirstRegistration.Value.Date : (DateTime?)null));
                 cfg.CreateMap<CarAdvertViewModel, CarAdvert>()
                    .ForMember(d => d.IsNew, opt => opt.MapFrom(src => src.New))
                    .ForMember(d => d.Fuel, opt => opt.MapFrom(src => Enum.Parse(typeof(FuelType), src.Fuel, true)))
-                   .ForMember(d => d.FirstRegistration, opt => opt.MapFrom(src => src.FirstRegistration.Value.Date));
+                   .ForMember(d => d.FirstRegistration, opt => opt.MapFrom(src => src.FirstRegistration.HasValue ? src.FirstRegistration.Value.Date : (DateTime?)null));
             });
             return config.CreateMapper();
         }
