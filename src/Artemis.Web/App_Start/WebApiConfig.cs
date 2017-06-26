e9754eaf1b72af2b9c2206e42c6f90f7c5c06678
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 07:57:46 +0200

    add basic web api project with nuget packages

---
+++ src/Artemis.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ModelBinding.Binders;
+
+namespace Artemis.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
