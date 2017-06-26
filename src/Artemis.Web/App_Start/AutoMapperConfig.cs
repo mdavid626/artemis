0199b81fd159e52a5da43a7c71c55a50a299d495
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-26 22:11:42 +0200

    use only date for the FirstRegistration field

--- src/Artemis.Web/App_Start/AutoMapperConfig.cs
+++ src/Artemis.Web/App_Start/AutoMapperConfig.cs
@@ -16,10 +16,12 @@ namespace Artemis.Web
             {
                 cfg.CreateMap<CarAdvert, CarAdvertViewModel>()
                    .ForMember(d => d.New, opt => opt.MapFrom(src => src.IsNew))
-                   .ForMember(d => d.Fuel, opt => opt.MapFrom(src => src.Fuel.ToString().ToLower()));
+                   .ForMember(d => d.Fuel, opt => opt.MapFrom(src => src.Fuel.ToString().ToLower()))
+                   .ForMember(d => d.FirstRegistration, opt => opt.MapFrom(src => src.FirstRegistration.Value.Date));
                 cfg.CreateMap<CarAdvertViewModel, CarAdvert>()
                    .ForMember(d => d.IsNew, opt => opt.MapFrom(src => src.New))
-                   .ForMember(d => d.Fuel, opt => opt.MapFrom(src => Enum.Parse(typeof(FuelType), src.Fuel, true)));
+                   .ForMember(d => d.Fuel, opt => opt.MapFrom(src => Enum.Parse(typeof(FuelType), src.Fuel, true)))
+                   .ForMember(d => d.FirstRegistration, opt => opt.MapFrom(src => src.FirstRegistration.Value.Date));
             });
             return config.CreateMapper();
         }
