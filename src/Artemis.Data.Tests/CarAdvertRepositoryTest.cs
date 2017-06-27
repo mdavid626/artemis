cd310479990e4f27ce1bb0385c28a7d327578174
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-27 14:14:23 +0200

    add tests for CarAdvertRepository

--- src/Artemis.Data.Tests/CarAdvertRepositoryTest.cs
+++ src/Artemis.Data.Tests/CarAdvertRepositoryTest.cs
@@ -18,7 +18,9 @@ namespace Artemis.Data.Tests
                 var dbContext = new CarAdvertDbContext(connection, false);
                 var carAdvert = new CarAdvert()
                 {
-                    Title = "Audi"
+                    Title = "Audi",
+                    Price = 1500,
+                    IsNew = true
                 };
                 dbContext.CarAdverts.Add(carAdvert);
                 dbContext.SaveChanges();
@@ -33,6 +35,8 @@ namespace Artemis.Data.Tests
 
                 // assert
                 Assert.AreEqual(firstItem.Title, carAdvert.Title);
+                Assert.AreEqual(firstItem.Price, carAdvert.Price);
+                Assert.AreEqual(firstItem.IsNew, carAdvert.IsNew);
             }
         }
 
@@ -45,7 +49,9 @@ namespace Artemis.Data.Tests
                 var dbContext = new CarAdvertDbContext(connection, false);
                 var carAdvert = new CarAdvert()
                 {
-                    Title = "Audi"
+                    Title = "Audi",
+                    Price = 1500,
+                    IsNew = true
                 };
                 dbContext.CarAdverts.Add(carAdvert);
                 dbContext.SaveChanges();
@@ -59,6 +65,118 @@ namespace Artemis.Data.Tests
 
                 // assert
                 Assert.AreEqual(item.Title, carAdvert.Title);
+                Assert.AreEqual(item.Price, carAdvert.Price);
+                Assert.AreEqual(item.IsNew, carAdvert.IsNew);
+            }
+        }
+
+        [TestMethod]
+        public void TestValidCreate()
+        {
+            using (var connection = Effort.DbConnectionFactory.CreateTransient())
+            {
+                // arrange
+                var dbContext = new CarAdvertDbContext(connection, false);
+
+                var dbContextProvider = Substitute.For<IDbContextProvider>();
+                dbContextProvider.Provide().Returns(p => dbContext);
+
+                using (var unitOfWork = new DbContextUnitOfWork(dbContextProvider))
+                {
+                    var carAdvert = new CarAdvert()
+                    {
+                        Title = "Audi",
+                        Price = 1500,
+                        IsNew = true
+                    };
+
+                    var repository = new CarAdvertRepository(unitOfWork);
+
+                    // act
+                    repository.Create(carAdvert);
+                    unitOfWork.Commit();
+
+                    // assert
+                    var item = dbContext.CarAdverts.Single();
+                    Assert.AreEqual(item.Title, carAdvert.Title);
+                    Assert.AreEqual(item.Price, carAdvert.Price);
+                    Assert.AreEqual(item.IsNew, carAdvert.IsNew);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestValidUpdate()
+        {
+            using (var connection = Effort.DbConnectionFactory.CreateTransient())
+            {
+                // arrange
+                var dbContext = new CarAdvertDbContext(connection, false);
+                var carAdvert = new CarAdvert()
+                {
+                    Title = "Audi",
+                    Price = 1500,
+                    IsNew = true
+                };
+                dbContext.CarAdverts.Add(carAdvert);
+                dbContext.SaveChanges();
+
+                var dbContextProvider = Substitute.For<IDbContextProvider>();
+                dbContextProvider.Provide().Returns(p => dbContext);
+
+                using (var unitOfWork = new DbContextUnitOfWork(dbContextProvider))
+                {
+                    var repository = new CarAdvertRepository(unitOfWork);
+                    var carAdvertToUpdate = repository.Get(carAdvert.Id);
+
+                    // act
+                    carAdvertToUpdate.Title = "Audi2";
+                    carAdvertToUpdate.Price = 1600;
+                    repository.Update(carAdvert);
+
+                    unitOfWork.Commit();
+
+                    // assert
+                    var item = dbContext.CarAdverts.Single();
+                    Assert.AreEqual(item.Title, "Audi2");
+                    Assert.AreEqual(item.Price, 1600);
+                    Assert.AreEqual(item.IsNew, true);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestValidDelete()
+        {
+            using (var connection = Effort.DbConnectionFactory.CreateTransient())
+            {
+                // arrange
+                var dbContext = new CarAdvertDbContext(connection, false);
+                var carAdvert = new CarAdvert()
+                {
+                    Title = "Audi",
+                    Price = 1500,
+                    IsNew = true
+                };
+                dbContext.CarAdverts.Add(carAdvert);
+                dbContext.SaveChanges();
+
+                var dbContextProvider = Substitute.For<IDbContextProvider>();
+                dbContextProvider.Provide().Returns(p => dbContext);
+
+                using (var unitOfWork = new DbContextUnitOfWork(dbContextProvider))
+                {
+                    var repository = new CarAdvertRepository(unitOfWork);
+                    var carAdvertToDelete = repository.Get(carAdvert.Id);
+
+                    // act
+                    repository.Delete(carAdvertToDelete);
+                    unitOfWork.Commit();
+
+                    // assert
+                    var isAny = dbContext.CarAdverts.Any();
+                    Assert.IsFalse(isAny);
+                }
             }
         }
     }
